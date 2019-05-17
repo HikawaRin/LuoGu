b8e52c187ccb6ca81e9f55e8a7934ea4b7bbfe96
RA <releaseavalon@163.com>
2019-05-17 14:00:46 +0800

    in P1013 change cstdio to iostream

--- P1013/P1013.cc
+++ P1013/P1013.cc
@@ -1,15 +1,27 @@
-#include<cstdio>
-
+#include<iostream>
+#include<string>
 using namespace std;
 
 #define MAX 12
-char table[MAX][MAX];
+string table[MAX][MAX];
 
 int main(){
+  #ifdef LOCAL
+    freopen("data.in", "r", stdin);
+  #endif
   int n;
-  scanf("%d", &n);
+  cin >> n;
   for (int i = 0; i < n; i++){
-    
+    for (int j = 0; j < n; j++){
+      cin >> table[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      cout << table[i][j] << ", ";
+    }
+    printf("\n");
   }
   return 0;
 }
